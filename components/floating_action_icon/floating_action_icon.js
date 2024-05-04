4b4655b9abea437fa1e9eb3094051f155dae2da1
crosage <90540469+crosage@users.noreply.github.com>
2024-05-04 11:07:51 +0800

    :sparkles: feat: floating_action_icon

---
+++ components/floating_action_icon/floating_action_icon.js
@@ -0,0 +1,27 @@
+// components/floating_action_icon/floating_action_icon.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: ""
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
