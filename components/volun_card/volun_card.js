f6e0ed2b722b53077613691a8180b32c82f653e1
crosage <90540469+crosage@users.noreply.github.com>
2024-06-26 22:16:54 +0800

    :sparkles: feat: 添加单项活动page

--- components/volun_card/volun_card.js
+++ components/volun_card/volun_card.js
@@ -1,4 +1,6 @@
 // components/volun_card/volun_card.js
+let drawQrcode = require("../../utils/weapp.qrcode.min");
+
 Component({
 
   /**
@@ -13,23 +15,66 @@ Component({
       type: String,
       value: ""
     },
-
+    activity_name: {
+      type: String,
+      value: ""
+    },
+    description_thumb: {
+      type: String,
+      value: ""
+    },
+    description: {
+      type: String,
+      value: ""
+    },
+    location: {
+      type: String,
+      value: ""
+    },
+    date: {
+      type: String,
+      value: ""
+    },
+    post_time: {
+      type: String,
+      value: ""
+    },
+    registration_deadline: {
+      type: String,
+      value: ""
+    },
+    organizer: {
+      type: String,
+      value: ""
+    },
+    creator: {
+      type: String,
+      value: ""
+    },
+    max_participants: {
+      type: Number,
+      value: 0
+    },
+    current_participants: {
+      type: Number,
+      value: 0
+    }
   },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+/**
+ * 组件的初始数据
+ */
+data: {
 
-  },
+},
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+/**
+ * 组件的方法列表
+ */
+methods: {
 
-  },
-  lifetimes: {
+},
+lifetimes: {
 
-  }
+}
 })
